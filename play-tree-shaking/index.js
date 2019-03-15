4a082297cccd12c92e1a62c1b390aac8aa3846d5
Clarence Huang <clarence.n.huang@gmail.com>
2019-03-14 21:54:15 -0700

    tree shaking shud w0rx

---
+++ play-tree-shaking/index.js
@@ -0,0 +1,4 @@
+import {fade_enter} from '../styled-components/index';
+
+
+fade_enter('a', 'b');
---
+++ play-tree-shaking/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        filename: 'output.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        usedExports: true
+    }
+};
